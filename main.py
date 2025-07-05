fc9d507ba349b0132f312bab9f871c8800b9749c
Yang Beining <2594405419@qq.com>
2025-07-05 05:53:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
 from sendmail import Mail, Wechat
 
 if __name__ == "__main__":
-    # mail = Mail(sender="2594405419@qq.com",
-    #             password="hoyuxzrdzxkmdihb",
-    #             reciver="2594405419@qq.com",
-    #             sender_name="act",
-    #             reciver_name="recive",
-    #             host="smtp.qq.com",
-    #             port=465)
-    # mail("test", "this is test")
-    # mail.save()
-    # we = Wechat("SCT69004TFjmqcjiOY0tMfCBtCr0y2Wsc.send", "测试:")
-    # # we("这是pip的测试")
-    # we.save()
-    # we = Wechat()
-    # we("你好啊")
-    # Wechat.P("测试classmethod", "RT")
-    # Mail.P("测试classmethod", 'gen_sh.py')
     Wechat.P("测试classmethod", 'gen_sh.py')
